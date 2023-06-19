b945ef247fa5c1efc33543e3a9525d7068854269
Hasnain-Mirrani07 <hmirrani3@gmail.com>
2023-06-19 12:02:53 +0500

    first commit

---
+++ lib/freeze/freeze_screen.dart
@@ -0,0 +1,46 @@
+
+
+import 'package:asif/freeze/person.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart' ;
+
+class FreezeScreen extends StatefulWidget {
+  const FreezeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FreezeScreen> createState() => _FreezeScreenState();
+}
+
+class _FreezeScreenState extends State<FreezeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.helloWorld),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+
+        var john = UserModel(firstName: 'John', lastName: 'Smith');
+        var person = Person(firstName: 'John', lastName: 'Smith');
+
+        var john2 = UserModel(firstName: 'John', lastName: 'Smith');
+
+        print( john.copyWith(firstName: 'as'));
+         print(john.firstName.toString());
+
+        print(person == john2);
+      }),
+    );
+  }
+}
+
+
+class Person{
+  String firstName ;
+  String lastName ;
+
+  Person({required this.firstName,required this.lastName}) ;
+
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:asif/freeze/freeze_screen.dart';
+import 'package:asif/widgets/dates_and_time_widget.dart';
+import 'package:asif/widgets/image_compressor_widget.dart';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart' ;
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+     // localizationsDelegates: AppLocalizations.localizationsDelegates,
+     // supportedLocales: AppLocalizations.supportedLocales,
+
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        AppLocalizations.delegate,
+        AppLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('es'), // Spanish
+      ],
+      home: ImageCompressorWidget(),
+    );
+  }
+
+
+}
